c3846f31658965460171b0487d7cedd90d06b8ec
Bishal Bose <bishalbose294@gmail.com>
2024-05-18 21:36:58 +0530

    Added candidate_job_match & resume_analyzer python files

---
+++ src/candidate_job_match.py
@@ -0,0 +1,18 @@
+
+
+class MatchJobCandidate:
+
+    def __init__(self) -> None:
+        pass
+
+
+    def compare(self, sent1, sent2):
+        pass
+
+    def match(self, jd, resume):
+        pass
+
+    def run(self, listOfJds, listOfResumes):
+        pass
+
+    pass
